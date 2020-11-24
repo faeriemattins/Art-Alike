9dcf6c2b935a681467183c5e19a882fc5da9ad2b
R. Faerie Mattins <67184157+faeriemattins@users.noreply.github.com>
2020-11-24 20:10:53 +0530

    Update views.py

--- my_app/views.py
+++ my_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'my_app/home.html')
+
+def login(request):
+    return render(request,'my_app/login.html')
