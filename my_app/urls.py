746964d53767a7d69a0f344d42fb8e8dd8d714ff
R. Faerie Mattins <67184157+faeriemattins@users.noreply.github.com>
2020-11-27 18:15:52 +0530

    Update urls.py

--- my_app/urls.py
+++ my_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='my_app-home'),
     path('login/', views.login, name='my_app-login'),
+    path('signup/', views.signup, name='my_app-signup'),
 ]
